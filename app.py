cadc7034fc18863d54900bffff8703988d25d746
naoya-iida <133567492+naoya-iida@users.noreply.github.com>
2023-05-28 14:02:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ if st.session_state["messages"]:
     messages = st.session_state["messages"]
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
-        speaker = "🙂"
+        speaker = "🗨️🙂"
         if message["role"]=="assistant":
-            speaker="😘"
+            speaker="🗨️😘"
 
         st.write(speaker + ": " + message["content"])
