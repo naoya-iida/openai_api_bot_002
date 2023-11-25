c16e929487937e1ca247128af3c1c8edf52df84f
naoya-iida <133567492+naoya-iida@users.noreply.github.com>
2023-11-25 20:47:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,11 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
+    # response = openai.ChatCompletion.create(
+    #    model="gpt-3.5-turbo",
+    #    messages=messages
+    response = openai.chat.completions.create(model="gpt-3.5-turbo",
+    messages=messages)  
     )  
 
     bot_message = response["choices"][0]["message"]
