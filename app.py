90f2c61168dbb30c5bc582bf351040a0eda6a2f3
naoya-iida <133567492+naoya-iida@users.noreply.github.com>
2023-11-25 22:54:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def communicate():
     messages=messages)  
 
     # bot_message = response["choices"][0]["message"]
-    bot_message = response.choices(0).message
-    # messages.append(bot_message)
-    print(bot_message)
+    bot_message = response.choices[0].message
+    messages.append(bot_message)
+    # print(bot_message)
     
     st.session_state["user_input"] = ""  # 入力欄を消去
 
